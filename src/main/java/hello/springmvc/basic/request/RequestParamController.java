6cc9b21eed72140fcc94fdd2e35657d988deef42
Rezal <rladlfdnd369@naver.com>
2024-08-01 10:56:35 +0900

    [6] SPRING MVC - HTTP 요청 파라미터 - v2~v5

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 
 /**
  * hello-form.html 에서 입력값 입력 후 post로 요청
- * */
+ */
 
 @Slf4j
 @Controller
@@ -27,13 +27,45 @@ public class RequestParamController {
         response.getWriter().write("ok");
     }
 
-    @ResponseBody  // @Controller 를 쓰면 return String 에서 viewName 을 찾는데, @ResponseBody 로 직접 body에 반환
-    @RequestMapping
+    // GET /request-param-v2?username=hello&age=20
+    @ResponseBody
+    // @Controller 를 쓰면 return String 에서 viewName 을 찾는데, @ResponseBody 로 직접 HTTP Message body 에 return 값 입력
+    @RequestMapping("/request-param-v2")
     public String requestParamV2(
             @RequestParam("username") String memberName,
             @RequestParam("age") int memberAge
     ) {
-        log.info("username={}, age={}", memberName, memberAge);
+//        log.info("username={}, age={}", memberName, memberAge);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3( // @RequestParam 의 파라미터(value) 생략
+            @RequestParam String username,
+            @RequestParam int age
+    ) {
+//        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    // 동일하게 작동하지만, 명시적으로 @RequestParam 을 쓰는 것을 권장
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(
+            String username,
+            int age
+    ) {
+//        log.info("username={}, age={}", username, age);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = true) String username,
+            @RequestParam(required = false) Integer age) { // int 로 선언 시, null 이 들어갈 수 없어 Integer 로(500 err)
+        log.info("username={}, age={}", username, age);
         return "ok";
     }
 }
